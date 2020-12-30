6aa8ba389793945e8f6f52d079be8da84010b6c4
cenfun <cenfun@gmail.com>
2020-12-30 21:37:58 +0800

    fix edit mode

---
+++ test/test.js
@@ -0,0 +1,12 @@
+const PSA = require("../lib");
+
+const url = "https://mp.weixin.qq.com/s/qBTUgtRDP9k7-FR50UBmBw";
+
+PSA({
+    url: url,
+    name: "zg-winter-girl",
+    type: "png",
+    edit: true,
+    scroll: true,
+    width: 1200
+});
