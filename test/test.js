e8b38457ddd9930014ccbd021bec3dc4837879af
cenfun <cenfun@gmail.com>
2020-12-30 23:00:20 +0800

    fix filename

--- test/test.js
+++ test/test.js
@@ -4,7 +4,7 @@ const url = "https://mp.weixin.qq.com/s/qBTUgtRDP9k7-FR50UBmBw";
 
 PSA({
     url: url,
-    name: "zg-winter-girl",
+    //name: "zg-winter-girl",
     type: "png",
     edit: true,
     scroll: true,
