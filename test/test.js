056dfcb9af5b204d3dda21c13d970176fc9e5a71
cenfun <cenfun@qq.com>
2022-06-29 18:28:42 +0800

    update deps

--- test/test.js
+++ test/test.js
@@ -1,17 +1,17 @@
-const PSA = require("../lib");
+const PSA = require('../lib');
 
-const url = "https://mp.weixin.qq.com/s/qBTUgtRDP9k7-FR50UBmBw";
+const url = 'https://mp.weixin.qq.com/s?__biz=Mzk0NjMwMDc1Nw==&mid=2247705994&idx=2&sn=bc505beb68d05253e9c34a9b23379cee&source=41';
 
 //const url = "http://www.chinaz.com/";
 
 PSA({
     url: url,
     //name: "zg-winter-girl",
-    type: "png",
+    type: 'png',
     edit: true,
     scroll: true,
     width: 1200,
-    output: "./output",
+    output: './output',
     delay: 1,
     timeout: 120
 });
